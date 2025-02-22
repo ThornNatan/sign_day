afb1a22d36f9487e0b4f57dc9243aa5b17e26cc7
ThornNatan <cruzrose715@gmail.com>
2025-02-22 10:29:51 -0600

    Add files via upload

---
+++ docs/sketch.js
@@ -0,0 +1,41 @@
+function setup() {
+  createCanvas(500, 500);
+}
+
+function draw() {
+  background(220);
+  fill ("#ADD8E6");
+  square (1,1,500)
+  fill ("white")
+  circle (80,40,100,100)
+  circle (30,10,100,100)
+  circle (10,80,100,100)
+  rect (223,366,20,200)
+  fill ("white")
+  circle (470,40,100,100)
+  circle (430,10,100,100)
+  circle (360,10,100,100)
+  rect (223,366,20,200)
+  fill ("black")
+  rect (1,300,500,300)
+  fill ("yellow")
+  rect (60,400,80,40)
+  fill ("yellow")
+  rect (220,400,80,40)
+  fill ("yellow")
+  rect (380,400,80,40)
+  fill ("#00FFFF")
+  fill ("yellow")
+  rect (220,400,80,40)
+  circle (232,220,300,300);
+  fill('Black');
+  textSize(40);
+  textStyle(BOLDITALIC);
+  text('Do not forget', 104, 210);
+  fill('black');
+  textSize(40);
+  textStyle(BOLDITALIC);
+  text('the milk!', 140, 260);
+  fill ("white")
+  rect (223,366,20,200)  
+}
